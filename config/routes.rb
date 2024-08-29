634eb954eedae018124cc1cad2a6a62c6722b107
André <andre.soares@altxtech.net>
2024-08-29 14:37:53 -0300

    Removing old playlists page that lived inside home

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :playlist_items
   resources :playlists
   root 'home#index'
-  get 'home/playlists'
   get 'videos/:id', to: 'home#show', as: 'video'
 end
